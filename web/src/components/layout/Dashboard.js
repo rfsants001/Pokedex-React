b08055d23eeaf8f6b2be0190fe9926018191987b
rfsants001 <rf.sants001@gmail.com>
2020-11-22 23:16:23 -0300

    Creat react app Pokedex

---
+++ web/src/components/layout/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import PokemonList from '../pokemon/PokemonList';
+
+export default class Dashboard extends Component {
+    render() {
+        return (
+            <div className="row">
+                <PokemonList/>
+            </div>
+        )
+    }
+}
